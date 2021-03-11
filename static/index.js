1f5e2049d128eced91531ffaa87f32b5777cd4b7
tautvydas.banevicius <tautvydas.banevicius@honestus.lt>
2021-03-11 17:18:59 +0200

    [IMP]

--- static/index.js
+++ static/index.js
@@ -3,7 +3,9 @@ $().ready(function() {
    var el = $(".bepulto");
    var el2 = $(".stay_tuned")
    var img = $(".img");
+   var btn = $(".butt_div");
    setTimeout(function(){
       el2.show('slow');
+      btn.show('slow');
     }, 4000);
 });
